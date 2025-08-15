83b441b9cf37b2b4d4dd03d7204607e9a569d4d5
Aaron Shirley <aaronshirley751@gmail.com>
2025-08-14 21:46:49 -0500

    test: update config test to new Settings; add strategy tests and stubbed scheduler test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
-from src.bot.config import Settings
+from src.bot.settings import Settings
 
 
 def test_settings_defaults():
     s = Settings()
-    assert s.ibkr_host == "127.0.0.1"
+    assert s.broker.host == "127.0.0.1"
